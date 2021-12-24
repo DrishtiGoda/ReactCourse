8cb99d990b21fb289a6a7d9370e6a144ee8196de
DrishtiGoda <goda.d@northeastern.edu>
2021-12-23 21:26:19 -0500

    Getting response from API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-onSearchSubmit(term) {
-  console.log(term);
-}
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID qIqk5ntpDPznqHIvu_6jt2csTZywrXpf9_dkI2-A8Tk ",
+      },
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit = {this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length} images
       </div>
     );
   }
