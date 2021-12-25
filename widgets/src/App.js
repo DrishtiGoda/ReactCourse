4c74c4c253bf5e515c913537f6eedbdc5a689b43
DrishtiGoda <goda.d@northeastern.edu>
2021-12-25 16:54:53 -0500

    Building and Styling the Accordion

---
+++ widgets/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title:'What is React?',
+        content:'React is a frontend javascript framework'
+    },
+    {
+        title:'Why use react?',
+        content:'React is a favorite JS library among engineers'
+    },
+    {
+        title:'How do you use React',
+        content:'You use react by creating components'
+    }
+]
+
+export default () => {
+  return (
+    <div>
+        <br/>
+      <Accordion items={items} />
+    </div>
+  );
+};
