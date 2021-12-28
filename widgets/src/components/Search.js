a022ab980aafac033406f082e67f9b00fae8c53b
DrishtiGoda <goda.d@northeastern.edu>
2021-12-27 19:50:05 -0500

    Default Search Items

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,12 +3,11 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
-
-  console.log("i run with every render");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -17,8 +16,12 @@ const Search = () => {
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
-    search();
+
+    if (term) {
+      search();
+    }
   }, [term]);
 
   return (
