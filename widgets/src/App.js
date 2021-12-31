a7ef4cbc938097e7e5dcee13692c1683ef74ad35
DrishtiGoda <goda.d@northeastern.edu>
2021-12-31 14:32:18 -0500

    Scaffolding the Translate Widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -35,6 +36,7 @@ const options = [
 
 export default () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     // <div>
@@ -43,12 +45,20 @@ export default () => {
     // <div>
     //   <Search />
     // </div>
+    // <div>
+    //   <button onClick={() => setShowDropdown(!showDropdown)}>
+    //     Toggle Dropdown
+    //   </button>
+    //   { showDropdown ? 
+    //     <Dropdown
+    //       onSelectedChange={setSelected}
+    //       selected={selected}
+    //       options={options}
+    //     /> : null
+    //   }
+    // </div>
     <div>
-      <Dropdown
-        onSelectedChange={setSelected}
-        selected={selected}
-        options={options}
-      />
+      <Translate/>
     </div>
   );
 };
